1434b9bc6a5f1646b68c90af63f673539b5e6021
Nzx03 <nzx786@gmail.com>
2024-10-27 20:10:12 +0530

    add files via upload

--- challenge.py
+++ challenge.py
@@ -1,11 +1,11 @@
 import random
 import time
 
-
-start=2
-finish=9
+print("Welcome to Timed Math challenge!")
+start=int(input("Enter the start value (e.g., 2):"))
+finish=int(input("Enter the start value (e.g., 45):"))
 oper=["+","-","*"]
-total_prob=10
+total_prob=int(input("Enter the total number of questions to solve:"))
 
 def generate():
    first=random.randint(start,finish)
@@ -14,28 +14,27 @@ def generate():
 
    exp= str(first) + operator + str(last)
    answer=eval(exp)
-#    print(exp,answer)
    return exp,answer
 
-# generate()
 start_time=time.time()
 wrong=0
 for i in range(total_prob):
    exp, answer=generate()
    while True:
-    guess=input("Problem No"+ " " + str(i+1) + ":" + exp + "=")
+    guess=input("Problem No"+ " " + str(i+1) + ":"+ " " + exp + "=")
     try:
            
            guess = int(guess)
            if guess == answer:
                break
            wrong+=1
+           print("Oops! Wrong answer, try again.")
     except ValueError:
            print("Please enter a valid number.")
 
 end_time=time.time()
 total_time=round(end_time-start_time)
-print("Nice try! You took",total_time,"seconds")
+print("Nice try! You took",total_time,"seconds.")
 
     
 
